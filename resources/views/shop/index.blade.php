147a57f499e36ad3788a7414ec1972bd4773dffa
PauloFranco <paulimsantos@gmail.com>
2022-04-01 17:56:43 -0300

    frontend index

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -12,13 +12,42 @@
 
 @section('content')
   
-<p>This is where the recipe will go</p
-
-    @foreach($ingredients as $ingredient)
-        <p>{{$ingredient}}</p>
+<h3>Available Ingredients</h3>
+<table class="table">
+    <thead>
+      <tr>
+        @foreach($ingredients->first()->getAttributes() as $key =>$attribute)
+            <th>{{$key}}</th>
+        @endforeach
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($ingredients as $ingredient)
+            <tr>
+                @foreach($ingredient->getAttributes() as $attribute)
+                    <td>{{$attribute}}</td>
+                @endforeach
+            </tr>
+        @endforeach
+    </tbody>
+  </table>
+  
+  <h2>Best score for a non-diet cookie is: {{$best_cookies[0]}}</h2>
+  <h3>Best recipe dosage for said cookie is:</h3>
+  <h4>
+    @foreach($best_cookies[1] as $key => $value)
+        {{$ingredients[$key]->name}} - {{$value}} spoons </br>
     @endforeach
-
-    <p> best cookies are {{$best_cookies}}
-    <p> best cookies are {{$best_diet_cookies}}
+   </h4>
+    </br>
+    </br>
+    <h2>Best score for a diet cookie is: {{$best_diet_cookies[0]}}</h2>
+    <h3>Best recipe dosage for said cookie is:</h3>
+    <h4>
+      @foreach($best_diet_cookies[1] as $key => $value)
+          {{$ingredients[$key]->name}} - {{$value}} spoons </br>
+      @endforeach
+     </h4>
+    
 
 @endsection
